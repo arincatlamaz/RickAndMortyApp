370b43946e5cd204a11dce70bbeff4f771ce6994
arincatlamaz <arincatlamaz@gmail.com>
2023-06-02 21:00:49 +0300

    .

--- app/src/main/java/com/arincatlamaz/rickandmortyapp/util/FirebaseDB.kt
+++ app/src/main/java/com/arincatlamaz/rickandmortyapp/util/FirebaseDB.kt
@@ -6,7 +6,11 @@ import android.widget.ImageButton
 import com.arincatlamaz.rickandmortyapp.R
 import com.arincatlamaz.rickandmortyapp.model.User
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 fun addToFB(context: Context, position: Int, name: String, favoriteBtn: ImageButton) {
@@ -17,7 +21,7 @@ fun addToFB(context: Context, position: Int, name: String, favoriteBtn: ImageBut
         val favoriteList = documentSnapshot.toObject(User::class.java)?.favoriteList
 
         if (favoriteList != null) {
-            val newItem = "$position: $name"
+            val newItem = "Position: $position - Name: $name"
             if (favoriteList.contains(newItem)) {
                 favoriteList.remove(newItem)
                 docRef.set(User(favoriteList))
@@ -40,7 +44,7 @@ fun addToFB(context: Context, position: Int, name: String, favoriteBtn: ImageBut
                     }
             }
         } else {
-            val newFavoriteList = arrayListOf("$position: $name")
+            val newFavoriteList = arrayListOf("Position: $position - Name: $name")
             docRef.set(User(newFavoriteList))
                 .addOnSuccessListener {
                     Log.d("SERIAL", "Favorite added")
